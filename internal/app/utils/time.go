c22bebe7455a11cd2b492253d207dd4cceb53e57
Caio Quintas <caioquintassantiago@gmail.com>
2024-02-21 20:12:10 -0300

    chore: new time utility

--- internal/app/utils/time.go
+++ internal/app/utils/time.go
@@ -6,3 +6,8 @@ func StringToTime(timeStr string) (time.Time, error) {
 	layout := "2006-01-02 15:04:05"
 	return time.Parse(layout, timeStr)
 }
+
+func TimeToString(time time.Time) string {
+	layout := "2006-01-02 15:04:05"
+	return time.Format(layout)
+}
